00cd17fa322a946bae000ba5e8bd87b94b14a03c
JamesMutahi <mutahijames0@gmail.com>
2019-07-23 12:37:51 +0300

    Launch

--- netflix/views.py
+++ netflix/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, Http404
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def home(request):
+    return render(request, 'home.html', locals())
